51296d727800959d052d41dbd52ed27b5be66cca
ThabisoTn <thabisongubanee@gmail.com>
2025-04-16 14:05:40 +0200

    Add project files.

---
+++ ProductAuthenticatorApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductAuthenticatorApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
